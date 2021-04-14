9d4026c1614540b87bbafefb2fa6eb2d1aaf59d8
Ragul-123 <81967281+Ragul-123@users.noreply.github.com>
2021-04-14 22:11:56 +0530

    HTML, CSS, JS Final

---
+++ login.js
@@ -0,0 +1,7 @@
+function getDetails() {  
+
+document.getElementById("details").style.display = "block";
+document.getElementById("name").value = document.getElementById("user_name").value;
+document.getElementById("email").value = document.getElementById("user_email").value;
+document.getElementById("address").value = document.getElementById("user_address").value;
+}
